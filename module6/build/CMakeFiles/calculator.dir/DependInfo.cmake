e4db0824b3d7bab559074be78db64ed19b5159d6
uberphoebus <87922930+uberphoebus@users.noreply.github.com>
2024-03-10 23:56:21 +0000

    cache

---
+++ module6/build/CMakeFiles/calculator.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/workspaces/cmake/module6/main.cpp" "/workspaces/cmake/module6/build/CMakeFiles/calculator.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../my_math/include"
+  "../my_print/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/workspaces/cmake/module6/build/my_math/CMakeFiles/my_math.dir/DependInfo.cmake"
+  "/workspaces/cmake/module6/build/my_print/CMakeFiles/my_print.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ module6/my_print/src/print_result.cpp
@@ -0,0 +1,6 @@
+#include "my_print/print_result.h"
+#include <iostream>
+
+void print_result(std::string result_type, float result_value) {
+  std::cout << result_type << " result:\t" << result_value << "\n";
+}
