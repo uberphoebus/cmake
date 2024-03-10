e1136f0cff06208ceb0396720dc0d3a28b5e652d
uberphoebus <87922930+uberphoebus@users.noreply.github.com>
2024-03-10 02:45:39 +0000

    set project name and version

--- module2/division.cpp
+++ module2/division.cpp
@@ -1,1 +1,1 @@
-float division(float num1, float num2) { return num1 / num2; }
+float division(float num1, float num2) { return num1 / num2 + 0; }
---
+++ module2/my_build_dir/CMakeFiles/calculator.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calculator.dir/addition.cpp.o"
+  "CMakeFiles/calculator.dir/division.cpp.o"
+  "CMakeFiles/calculator.dir/main.cpp.o"
+  "CMakeFiles/calculator.dir/print_result.cpp.o"
+  "calculator"
+  "calculator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/calculator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
